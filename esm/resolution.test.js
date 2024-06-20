978170ae8579da1a5cf033d29738e0b671f55dcc
Argyris <arkatsy@gmail.com>
2024-06-20 15:45:37 +0300

    chore: remove redundant assertion

--- esm/resolution.test.js
+++ esm/resolution.test.js
@@ -14,7 +14,7 @@ describe("ECMA_262_6_1_7_ARRAY_INDEX", () => {
   });
 
   it("should reject float numbers", () => {
-    strictEqual(ECMA_262_6_1_7_ARRAY_INDEX(0.1), false);
+    strictEqual(ECMA_262_6_1_7_ARRAY_INDEX(Math.random()), false);
   });
 
   it("should reject negative numbers", () => {
@@ -23,7 +23,6 @@ describe("ECMA_262_6_1_7_ARRAY_INDEX", () => {
 
   it("should reject integers that are not inside the array indices range", () => {
     strictEqual(ECMA_262_6_1_7_ARRAY_INDEX(-1), false);
-    strictEqual(ECMA_262_6_1_7_ARRAY_INDEX(0.1), false);
     strictEqual(ECMA_262_6_1_7_ARRAY_INDEX(2 ** 32), false);
     strictEqual(ECMA_262_6_1_7_ARRAY_INDEX(-0), false);
   })
