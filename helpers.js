1a76ab62b579cb919e09c7585b07711c01830927
Argyris <arkatsy@gmail.com>
2024-06-21 00:11:13 +0300

    feat: improve isValidURL helper

--- helpers.js
+++ helpers.js
@@ -2,17 +2,24 @@
  * @param {string} specifier
  * @returns {boolean} Whether the specifier is a valid URL or not
  * @see {@link https://nodejs.org/docs/latest/api/esm.html#urls} Nodejs docs
- * @description Checks if the specifier is a valid URL or not
+ * @description Checks if the specifier is a valid URL or not.
  */
 function isValidURL(specifier) {
   const validProtocols = ["file:", "data:", "node:"];
+
+  try {
+    new URL(specifier);
+  } catch (e) {
+    return false;
+  }
+
   return validProtocols.some((v) => specifier.startsWith(v));
 }
 
 /**
- * 
- * @param {string} str 
- * @param {string[]} arr 
+ *
+ * @param {string} str
+ * @param {string[]} arr
  * @returns {boolean} Whether the string includes any of the elements in the array
  */
 function includesAny(str, arr) {
